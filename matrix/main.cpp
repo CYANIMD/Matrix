ed8656d2325f15490031d9da9e544c4909c899ab
Игнатьев Михаил <CYANIMD@gmail.com>
2025-01-18 00:38:38 +0300

    Реализовал методы reverse_cols(), get_submatrix(), inverse_matrix(). Исправил метод generate_indentity_matrix().

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -1,11 +1,12 @@
-﻿
-#include "matrix.h"
+﻿#include "matrix.h"
 #include "entity.h"
+#include <vector>
+
+using namespace IMD;
 
 int main() {
-	auto mrx = IMD::generate_indentity_matrix<long>(3);
-	mrx(2, 1) = -43;
-	mrx(1, 1) = 5;
-	println(mrx);
-	std::cout << rang(mrx);
+	matrix<int> mrx1 = generate_random_matrix(3, 3, -2, 2);
+	println(mrx1);
+	reverse_cols(mrx1);
+	println(mrx1);
 }
--- matrix/matrix.h
+++ matrix/matrix.h
@@ -30,7 +30,9 @@ namespace IMD {
 	class matrix {
 	private:
 		ValueType** _data;
+		//Число строк.
 		size_t _rows;
+		//Число столбцов.
 		size_t _cols;
 	public:
 		//Простой конструктор.
@@ -102,11 +104,17 @@ namespace IMD {
 			return _data[row][col];
 		}
 		//Возвращает число строк.
-		constexpr size_t rows() const { return _rows; }
+		constexpr size_t rows() const {
+			return _rows;
+		}
 		//Возвращает число столбцов.
-		constexpr size_t cols() const { return _cols; }
+		constexpr size_t cols() const {
+			return _cols;
+		}
 		//Возвращает количество элементов.
-		constexpr size_t size() const { return cols() * rows(); };
+		constexpr size_t size() const {
+			return cols() * rows();
+		};
 		//Очищение матрицы.
 		void clear() {
 			_rows = (0);
@@ -114,7 +122,9 @@ namespace IMD {
 			free_data();
 		}
 		//Проверяет, пуста ли матрица.
-		constexpr bool empty() const { return _data == nullptr; }
+		constexpr bool empty() const {
+			return _data == nullptr && rows() == 0 && cols() == 0;
+		}
 		//Заполнение матрицы заданным значением.
 		void fill(const ValueType& value) {
 			for (size_t i{ 0 }; i < rows(); ++i)
@@ -149,6 +159,10 @@ namespace IMD {
 			delete[] _data;
 			_data = nullptr;
 		}
+		ValueType*& get_row(size_t row) {
+			if (row > rows()) throw std::invalid_argument("row");
+			return _data[row];
+		}
 	public:
 		//Транспонирование матрицы произвольного размера.
 		friend void transpose(matrix<ValueType>& mrx) {
@@ -158,7 +172,7 @@ namespace IMD {
 				for (size_t j{ 0 }; j < mrx.rows(); ++j)
 					new_data[i][j] = mrx._data[j][i];
 			}
-			mrx.free_data;
+			mrx.free_data();
 			std::swap(mrx._data, new_data);
 			std::swap(mrx._rows, mrx._cols);
 		}
@@ -176,15 +190,62 @@ namespace IMD {
 			}
 		}
 		//Инвертирует строки матрицы.
-		friend void reverse_rows(matrix< ValueType>& mrx) {
+		friend void reverse_rows(matrix<ValueType>& mrx) {
 			for (size_t i{ 0 }; i < mrx.rows(); ++i)
 				std::reverse(mrx._data[i], mrx._data[i] + mrx.cols());
 		}
 		//Инвертирует столбцы матрицы.
-		friend void reverse_cols(matrix< ValueType>& mrx) {
+		friend void reverse_cols(matrix<ValueType>& mrx) {
+			for (size_t j{ 0 }; j < mrx.cols(); ++j)
+				for (size_t i{ 0 }; i < mrx.rows() / 2; ++i) {
+					std::swap(mrx._data[i][j], mrx._data[mrx.rows() - i - 1][j]);
+				}
 
 		}
 	};
+	//Возвращает подматрицу исходной матрицы без строки и столбца с указанными индексами.
+	template<typename T>
+	matrix<T> get_submatrix(const matrix<T>& mrx, size_t row, size_t col) {
+		size_t rows{ mrx.rows() };
+		size_t cols{ mrx.cols() };
+		if (row >= rows) throw std::invalid_argument("The matrix doesn't have this row.");
+		if (col >= cols) throw std::invalid_argument("The matrix doesn't have this col.");
+		matrix<T> result{ rows - 1, cols - 1 }; //Подматрица исходной матрицы.
+		for (size_t main_i{ 0 }, sub_i{ 0 }; main_i < rows; ++main_i) {
+			if (main_i == row) continue;
+			for (size_t main_j{ 0 }, sub_j{ 0 }; main_j < cols; ++main_j) {
+				if (main_j == col) continue; // Пропускаем столбец, который нужно исключить
+				result(sub_i, sub_j) = mrx(main_i, main_j);
+				sub_j++;
+			}
+			sub_i++;
+		}
+		return result;
+
+	}
+	//Возвращает указатель на обратную матрицу.
+	//Происходит выделение динамической памяти.
+	template<Numeric T0, Numeric T1>
+	matrix<T1>* inverse_matrix(const matrix<T0>& input) {
+		if (input.rows() != input.cols()) return nullptr;
+		size_t n = input.rows();
+		matrix<T1>* result = new matrix<T1>{ n, n};
+		double det = determinate(input);
+		if (det == 0) return nullptr;
+
+		for (size_t i{ 0 }; i < n; ++i) {
+			for (size_t j{ 0 }; j < n; ++j) {
+				auto minor = get_submatrix(input, i, j);
+				result->operator()(i,j) = determinate(minor);
+				if ((i + j) % 2 == 1) {
+					result->operator()(i, j) *= -1;
+				}
+				result->operator()(i, j) /= det;
+			}
+		}
+		transpose(*result);
+		return result;
+	}
 	//Печать в консоль.
 	template<typename ValueType>
 	void print(const IMD::matrix<ValueType>& mrx, const char* separator = " ") {
@@ -200,34 +261,15 @@ namespace IMD {
 		print(mrx, separator);
 		std::cout << std::endl;
 	}
-	//template<Numeric T>
-	//bool inverse_matrix(const matrix<T>& input, matrix<T>& output) {
-	//	if (input.rows() != input.cols()) return false;
-
-	//	matrix<T> gauss_matrix{ input.rows() * 2, input.cols() * 2 }; //Создание матрицы (input | E)
-	//	size_t current_row{ 0 };
-	//	for (size_t i{ 0 }; i < input.cols(); ++i) {
-	//		T current_value = input[current_row][i];
-	//		for (size_t j{ 0 }; j < input.rows(); ++j) {
-	//			if (j != current_row) {
-	//				input[][] += 
-	//			}
-	//		}
-	//	}
-	//}
-	// 
 	//Возвращает определитель квадратной матрицы.
 	template<Numeric T>
 	T determinate(const matrix<T>& mrx) {
 		if (mrx.rows() != mrx.cols()) throw std::invalid_argument("The matrix isn't square");
-
 		T result{ 0 };
 		size_t rows{ mrx.rows() };
-
 		//Загатовки для вычисления детерминантов матриц размера 1x1 и 2x2.
 		if (rows == 1) return mrx(0, 0);
 		if (rows == 2) return mrx(0, 0) * mrx(1, 1) - mrx(0, 1) * mrx(1, 0);
-
 		for (size_t i{ 0 }; i < rows; ++i) {
 			matrix<T> minor(rows - 1, rows - 1); //Создание минора матрицы.
 			for (size_t j{ 1 }; j < rows; ++j) {
@@ -249,18 +291,14 @@ namespace IMD {
 		size_t result{ 0 }; //Ранг матрицы.
 		matrix<T> copy_mrx{ mrx };
 		for (size_t i{ 0 }; i < mrx.rows(); ++i) {
-			//Находим ведущий элемент в текущей строке
-			size_t lead = 0;
+			size_t lead = 0; //Находим ведущий элемент в текущей строке.
 			while (lead < mrx.cols() && copy_mrx(i, lead) == T(0)) ++lead;
-
 			if (lead < mrx.cols()) {
-				++result; // Увеличиваем ранг
-				//Приводим строку к единичному виду
-				for (size_t r{ i + 1 }; r < mrx.rows(); ++r) {
+				++result;
+				for (size_t r{ i + 1 }; r < mrx.rows(); ++r) { //Приводим строку к единичному виду.
 					if (copy_mrx(r, lead) != T(0)) {
-						T factor = mrx(r, lead) / mrx(i, lead);
 						for (size_t c{ lead }; c < mrx.cols(); ++c)
-							copy_mrx(r, c) -= factor * copy_mrx(i, c);
+							copy_mrx(r, c) -= copy_mrx(i, c) * mrx(r, lead) / mrx(i, lead);
 					}
 				}
 			}
@@ -300,6 +338,81 @@ namespace IMD {
 		}
 		return result;
 	}
+	//Возвращает матрица заданного размера, заполненную случайный целыми числами в диапазоне [min_value; max_value).
+	template<Integral T>
+	inline matrix<T> generate_random_matrix(size_t rows, size_t cols, T min_value, T max_value) {
+		std::srand(std::time(nullptr));
+		matrix<int> result{ rows, cols };
+		for (size_t i{ 0 }; i < rows; ++i)
+			for (size_t j{ 0 }; j < cols; ++j)
+				result(i, j) = std::rand() % (max_value - min_value + 1) + min_value;
+		return result;
+	}
+	//Возвращает матрица заданного размера, заполненную случайный вещественными числами в диапазоне [min_value; max_value).
+	template<Floating T>
+	inline matrix<T> generate_random_matrix(size_t rows, size_t cols, T min_value, T max_value) {
+		matrix<T> result{ rows, cols };
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_real_distribution dist(min_value, max_value);
+		for (size_t i{ 0 }; i < rows; ++i)
+			for (size_t j{ 0 }; j < cols; ++j) {
+				auto x = dist(gen);
+				result(i, j) = x;
+			}
+		return result;
+	}
+	//Возвращает квадратную спиральную матрицу, заполненную целыми числами.
+	template<Integral T = short>
+	inline matrix<T> generate_spiral_matrix(size_t n) {
+		matrix<T> result{ n, n };
+		long left{ 0 };
+		long right(n - 1);
+		long bottom(n - 1);
+		long top{ 0 };
+		T value{ 1 };
+
+		while (left <= right && top <= bottom) {
+			for (long i{ left }; i <= right; ++i) {
+				result(top, i) = value;
+				++value;
+			}
+			++top;
+			for (int i{ top }; i <= bottom; ++i) {
+				result(i, right) = value;
+				++value;
+			}
+			--right;
+			if (top <= bottom) {
+				for (int i{ right }; i >= left; --i) {
+					result(bottom, i) = value;
+					++value;
+				}
+				--bottom;
+			}
+			if (left <= right) {
+				for (int i{ bottom }; i >= top; --i) {
+					result(i, left) = value;
+					++value;
+				}
+				++left;
+			}
+		}
+		return result;
+	}
+	//Возвращает единичную матрицу размера nxn.
+	template<Numeric T = short>
+	inline matrix<T> generate_indentity_matrix(size_t n) {
+		matrix<T> result{ n, n, T(0) };
+		--n;
+		for (size_t i{ 0 }; i < n; ++i) result(i, i) = 1;
+		return result;
+	}
+	//Возвращает нулевую матрицу размера nxn.
+	template<Integral T = short>
+	inline matrix<T> generate_zero_matrix(size_t n) {
+		return matrix<T>{n, n, T(0)};
+	}
 	template<typename ValueType, typename P>
 	matrix<ResultTypeProduct<ValueType, P>> operator*(const matrix<ValueType>& mrx, const P& value) {
 		matrix<ValueType> result{ mrx };
@@ -337,7 +450,7 @@ namespace IMD {
 	template<typename T0, typename T1>
 	matrix<ResultTypeSum<T0, T1>> operator+(const matrix<T1>& first, const matrix<T0>& second) {
 		if (first.rows() != second.rows() || first.cols() != second.cols())
-			throw std::invalid_argument("The matrixes have uncorrect size");
+			throw std::invalid_argument("The matrices have uncorrect size");
 		matrix<ResultTypeSum<T0, T1>> result{ first.rows(), first.cols() };
 		for (size_t i{ 0 }; i < result.rows(); ++i) {
 			for (size_t j{ 0 }; j < result.cols(); ++j)
@@ -352,87 +465,30 @@ namespace IMD {
 	template<typename T0, typename T1> requires requires(T0 a, T1 b) { { a + b } -> std::same_as<T1>; }
 	matrix<T1>& operator+=(matrix<T1>& first, const matrix<T0>& second) {
 		if (first.rows() != second.rows() || first.cols() != second.cols())
-			throw std::invalid_argument("The matrixes have uncorrect size");
+			throw std::invalid_argument("The matrices have uncorrect size");
 		first = first + second;
 		return first;
 	}
-	template<typename T0, typename T1>requires requires(T0 a, T1 b) { { a - b } -> std::same_as<T1>; }
+	template<typename T0, typename T1> requires requires(T0 a, T1 b) { { a - b } -> std::same_as<T1>; }
 	matrix<T1>& operator-=(matrix<T0>& first, const matrix<T1>& second) {
 		return first += (-1) * second;
 	}
-	//Возвращает матрица заданного размера, заполненную случайный целыми числами в диапазоне [min_value; max_value).
-	template<Integral T>
-	inline matrix<T> generate_random_matrix(size_t rows, size_t cols, T min_value, T max_value) {
-		std::srand(std::time(nullptr));
-		matrix<int> result{ rows, cols };
-		for (size_t i{ 0 }; i < rows; ++i)
-			for (size_t j{ 0 }; j < cols; ++j)
-				result(i, j) = std::rand() % max_value + min_value;
+	template<typename ValueType0, typename ValueType1>
+	matrix<ResultTypeSum<ResultTypeProduct<ValueType0, ValueType1>, ResultTypeProduct<ValueType0, ValueType1>>> operator*(const matrix<ValueType0>& first, const matrix<ValueType1>& second) {
+		if (first.cols() != second.rows())
+			throw std::invalid_argument("The matrices are not the correct size");
+		matrix<ResultTypeSum<ResultTypeProduct<ValueType0, ValueType1>, ResultTypeProduct<ValueType0, ValueType1>>> result{ first.rows(), second.cols() };
+		for (size_t i{ 0 }; i < first.rows(); ++i)
+			for (size_t j{ 0 }; j < second.cols(); ++j)
+				for (size_t k = 0; k < first.cols(); ++k)
+					result(i, j) += first(i, k) * second(k, j);
 		return result;
 	}
-	//Возвращает матрица заданного размера, заполненную случайный вещественными числами в диапазоне [min_value; max_value).
-	template<Floating T>
-	inline matrix<T> generate_random_matrix(size_t rows, size_t cols, T min_value, T max_value) {
-		matrix<T> result{ rows, cols };
-		std::random_device rd;
-		std::mt19937 gen(rd());
-		std::uniform_real_distribution dist(min_value, max_value);
-		for (size_t i{ 0 }; i < rows; ++i)
-			for (size_t j{ 0 }; j < cols; ++j) {
-				auto x = dist(gen);
-				result(i, j) = x;
-			}
-		return result;
+	template<typename ValueType0, typename ValueType1>
+	matrix<ValueType0>& operator*=(matrix<ValueType0>& first, const matrix<ValueType1>& second) {
+		first = first * second;
+		return first;
 	}
-	//Возвращает квадратную спиральную матрицу, заполненную целыми числами.
-	template<Integral T>
-	inline matrix<T> generate_spiral_matrix(size_t n) {
-		matrix<T> result{ n, n };
-		long left{ 0 };
-		long right ( n - 1 );
-		long bottom( n - 1 );
-		long top{ 0 };
-		T value{ 1 };
-
-		while (left <= right && top <= bottom) {
-			for (long i{ left }; i <= right; ++i) {
-				result(top, i) = value;
-				++value;
-			}
-			++top;
-			for (int i{ top }; i <= bottom; ++i) {
-				result(i, right) = value;
-				++value;
-			}
-			--right;
-			if (top <= bottom) {
-				for (int i{ right }; i >= left; --i) {
-					result(bottom, i) = value;
-					++value;
-				}
-				--bottom;
-			}
-			if (left <= right) {
-				for (int i{ bottom }; i >= top; --i) {
-					result(i, left) = value;
-					++value;
-				}
-				++left;
-			}
-		}
-		return result;
-	}
-	//Возвращает единичную матрицу размера nxn.
-	template<Integral T>
-	inline matrix<T> generate_indentity_matrix(size_t n) {
-		return matrix<T>{n, n, T(1)};
-	}
-	//Возвращает нулевую матрицу размера nxn.
-	template<Integral T>
-	inline matrix<T> generate_zero_matrix(size_t n) {
-		return matrix<T>{n, n, T(0)};
-	}
-
 }
 
 
